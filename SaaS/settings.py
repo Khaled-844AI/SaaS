fed18b7e7e7112877a18772dda406c28347998d9
Mouad <mouadkhaled2004@gmail.com>
2024-09-03 10:43:15 +0100

    fix

--- SaaS/settings.py
+++ SaaS/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*(+d8z7rb*2l@5v-f=6b^pxfl#ivp=!_29dii-uj90+6c&r@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app"
