89ec8c65de40de80a96f32a3e7e5ae8f16b05b5a
Mouad <mouadkhaled2004@gmail.com>
2024-09-15 11:22:34 +0100

    added subscription plans

--- helpers/billing.py
+++ helpers/billing.py
@@ -87,3 +87,12 @@ def get_subscription(stripe_id, raw=True):
     if raw:
         return response
     return response.url
+
+def get_checkout_customer_plan(session_id):
+    checkout_response = get_checkout_session(session_id, raw=True)
+    customer_id = checkout_response.customer
+    sub_stripe_id = checkout_response.subscription
+    sub_r = get_subscription(sub_stripe_id, raw=False)
+    sub_plan = sub_r.plan
+
+    return customer_id, sub_plan.id
